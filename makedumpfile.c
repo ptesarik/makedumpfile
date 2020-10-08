61518e922fc171792c1fdbf69dbf4f3aaeb348c8
Petr Tesarik <ptesarik@suse.com>
2020-10-08 20:13:19 +0200

    Remove (unused) cache code
    Since the original cache code is no longer used, it can be removed
    completely.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- makedumpfile.c
+++ makedumpfile.c
@@ -19,7 +19,6 @@
 #include "elf_info.h"
 #include "erase_info.h"
 #include "sadump_info.h"
-#include "cache.h"
 #include <stddef.h>
 #include <ctype.h>
 #include <sys/time.h>
@@ -3544,9 +3543,6 @@ out:
 		if (!fallback_to_current_page_size())
 			return FALSE;
 	}
-
-	if (!is_xen_memory() && !cache_init())
-		return FALSE;
 
 	if (info->flag_mem_usage && !get_kcore_dump_loads())
 		return FALSE;
@@ -9123,9 +9119,6 @@ out:
 		if (!fallback_to_current_page_size())
 			return FALSE;
 	}
-
-	if (!cache_init())
-		return FALSE;
 
 	if (xen_info_required == TRUE) {
 		if (!get_xen_info())
